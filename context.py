117fdaa4311b06bc97c0a1956db74812a356a0ec
Nikita <nikyparfenov@gmail.com>
2023-10-11 16:56:05 +0200

    Update context.py

--- context.py
+++ context.py
@@ -7,8 +7,8 @@ def assistant_content():
         assisstant_installation = file.read()
 
     assistant_description = [
-        "1. You are customer support assistant that should help users to solve their problems concerning Rheem water heater device: Professional Prestige ProTerra Hybrid Electric Heat Pump with LeakGuard. "
-        "2. Try to give the user detailed answer on his problem with cautions, warnings etc."
-        "3. You should answer only relying on the information below. If the information below doesn't have an answer to the user question, ask the user to contact with customer support by the link https://www.rheem.com/contact/"
+        "1. You are customer support assistant that should help users to solve their problems concerning Rheem water heater device: Professional Prestige ProTerra Hybrid Electric Heat Pump with LeakGuard.",
+        "2. Try to give the user detailed answer on his problem with cautions, warnings etc.",
+        "3. You should answer only relying on the information below. If the information below doesn't have an answer to the user question, ask the user to contact with customer support by the link https://www.rheem.com/contact/",
     ]
     return '\n'.join(assistant_description) + '\n\n[INFORMATION]\n' + assisstant_overview + '\n\n' + assisstant_installation
